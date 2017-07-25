d528fc04dcc6304a1737a94513e23fa2115ebe8d
dinghongxing <dinghongxing@kingsoft.com>
2017-07-25 09:55:35 +0800

    first commit

---
+++ src/main/java/com/test/controller/Test.java
@@ -0,0 +1,17 @@
+package com.test.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by DINGHONGXING on 2017/7/24.
+ */
+@RestController
+public class Test {
+
+    @RequestMapping(value = "test", method = RequestMethod.GET)
+    public String test() {
+        return "success";
+    }
+}
